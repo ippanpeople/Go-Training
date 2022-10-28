fee4caaec85cb3cc9c194a330ab725f44f99647a
rinriN <2210368@ecc.ac.jp>
2022-10-28 23:28:41 +0900

    :memo: : use deck.go deck type

--- p1_cards/main.go
+++ p1_cards/main.go
@@ -1,19 +1,32 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
-	card := newCard()
+	// card := newCard()
 
-	fmt.Println(card)
+	// fmt.Println(card)
 
-	cards := []string{"Ace of Spades", newCard(), newCard()}
+	// cards := []string{"Ace of Spades", newCard()}
+	cards := deck{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(cards)
+	// fmt.Println(cards)
+	// fmt.Println(cards[1])
+	// fmt.Println(cards[0:2])
+	// fmt.Println(cards[:2])
+
+	// for i := 0; i < len(cards); i++ {
+	// 	fmt.Println(cards[i])
+	// }
+
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+
+	cards.print()
 }
 
 func newCard() string {
-	return "Ace of Spades"
+	return "Two of Spades"
 }

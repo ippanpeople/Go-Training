406c439bbbe30908a91aa24ea938e764e9231d00
rinriN <2210368@ecc.ac.jp>
2022-10-28 11:59:00 +0900

    :memo: : init function and return val for init var

--- p1_cards/main.go
+++ p1_cards/main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	// card := "Ace of Spades"
+	card := newCard()
 
 	fmt.Println(card)
 
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
